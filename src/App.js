8ee37c173e68c7df6b722c454def67debb165926
Bahaa Hashim <bahaahashim96@gmail.com>
2021-02-05 17:17:01 +0200

    toDoList

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ToDoComponent from './Components/ToDoComponent.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <ToDoComponent/>
+    </div>
+  );
+}
+
+export default App;
